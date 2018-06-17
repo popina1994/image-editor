bf542fb5dd083aeeba23c4bc81e93cc087861c8a
Djordje Zivanovic <djordjezivanovicva@gmail.com>
2018-06-17 22:54:43 +0200

    1. Added loading of picture from file.

--- src/rs/ac/bg/etf/zd173013m/gui/Application.scala
+++ src/rs/ac/bg/etf/zd173013m/gui/Application.scala
@@ -3,19 +3,48 @@ package rs.ac.bg.etf.zd173013m.gui
 import java.awt.Toolkit._
 
 import javax.swing.ImageIcon
+import java.io.File
 
 import scala.swing._
+import scala.swing.event._
 
 object Application extends SimpleSwingApplication {
   def top = new MainFrame {
+    //TODO: Submenu/ open
     title = "Image Editor"
     preferredSize = getDefaultToolkit.getScreenSize
+    private val pictureLabel = new Label{
+      icon = new ImageIcon("C:/Users/popina/IdeaProjects/ImageEditor/assets/1.jpg")
+      reactions += {
+        case MouseClicked(_, point, _, _, _) => { }
+      }
+    }
     contents = new FlowPanel {
       contents += new Button {
         text = "touch me"
       }
-      contents += new Label{
-        icon = new ImageIcon("C:/Users/popina/IdeaProjects/ImageEditor/assets/2.png")
+      contents += pictureLabel
+    }
+    menuBar = new MenuBar {
+      contents += new Menu("File") {
+        contents += new MenuItem(new Action("Open")
+          {
+            def apply
+            {
+              println("Open")
+              val chooser = new FileChooser(new File("C:/Users/popina/IdeaProjects/ImageEditor/assets/"))
+              val result = chooser.showOpenDialog(null)
+              if (result == FileChooser.Result.Approve) {
+                println(chooser.selectedFile)
+                pictureLabel.icon = new ImageIcon(chooser.selectedFile.getAbsolutePath)
+              }
+            }
+          })
+        contents += new MenuItem(new Action("Save") {
+          override def apply(): Unit = {
+            println("Save")
+          }
+        })
       }
     }
   }
--- src/rs/ac/bg/etf/zd173013m/gui/ImageLabel.scala
+++
@@ -1,9 +0,0 @@
-package rs.ac.bg.etf.zd173013m.gui
-
-import scala.swing._
-import javax.swing.{ImageIcon, JLabel}
-
-class ImageLabel(imagePath : String)  {
-  val imageIcon: ImageIcon = new ImageIcon(imagePath)
-  val imageLabel = new JLabel(imageIcon)
-}
